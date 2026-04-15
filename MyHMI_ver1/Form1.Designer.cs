bed4c8dd261ef69006d4e213646a25b5546e3d1c
Santiago <SantiMF20@proton.me>
2026-04-15 00:34:43 -0600

    Agregar archivos de proyecto.

---
+++ MyHMI_ver1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MyHMI_ver1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            richTextBox4 = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(23, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(146, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(41, 94);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(710, 139);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(41, 275);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(157, 120);
+            richTextBox2.TabIndex = 3;
+            richTextBox2.Text = "RT2";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(348, 275);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(157, 120);
+            richTextBox3.TabIndex = 4;
+            richTextBox3.Text = "RT3";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(594, 275);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(157, 120);
+            richTextBox4.TabIndex = 5;
+            richTextBox4.Text = "RT4";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(91, 252);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Temperature";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(388, 252);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Pressure";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(648, 252);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Flow";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(richTextBox4);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ MyHMI_ver1/Form1.cs
@@ -0,0 +1,202 @@
+using System.Net.Sockets;
+
+namespace MyHMI_ver1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Link the following functions
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread connection = new Thread(Connect2Server);
+            connection.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sendMessage();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UIavilable = false;
+            disconnectClient();
+        }
+
+        // UI handler functions
+        bool UIavilable = true;
+        private void clearTextboxFromSubprocess(RichTextBox textBox)
+        {
+            if (UIavilable)
+            {
+                Invoke(new MethodInvoker(() =>
+                {
+                    textBox.Text = "";
+                }));
+            }
+        }
+        private void write2TextboxFromSubprocess(RichTextBox textBox, string text)
+        {
+            if (UIavilable)
+            {
+                Invoke(new MethodInvoker(() =>
+                {
+                    if (UIavilable) textBox.Text += "\n" + text;
+                }));
+            }
+        }
+        private void enableButtonFromSubprocess(Button button)
+        {
+            if (UIavilable)
+            {
+                Invoke(new MethodInvoker(() =>
+                {
+                    button.Enabled = true;
+                }));
+            }
+        }
+        private void disableButtonFromSubprocess(Button button)
+        {
+            if (UIavilable)
+            {
+                Invoke(new MethodInvoker(() =>
+                {
+                    button.Enabled = false;
+                }));
+            }
+        }
+        //client functions
+        TcpClient client = null;
+        bool keepConnection = false;
+        private void Connect2Server()
+        {
+            try
+            {
+                disableButtonFromSubprocess(button1);
+                client = new TcpClient("127.0.0.1", 502);
+                if (client.Connected)
+                {
+                    keepConnection = true;
+                    NetworkStream stream = client.GetStream();
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[1024];
+                    while (keepConnection && client.Connected)
+                    {
+                        if (!stream.DataAvailable)
+                        {
+                            bytesRead = 0;
+                        }
+                        bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        if (bytesRead == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (buffer[2] == 0 && buffer[3] == 0)
+                            {//checking Protocol iddentifier
+
+                                write2TextboxFromSubprocess(richTextBox1, "Protocol ID OK");
+                                Int16 ExpectedBytes = (Int16)(3 + RegQty * 2);
+                                if (buffer[4] == (byte)ExpectedBytes >> 8 && buffer[5] == (byte)ExpectedBytes)
+                                {// Checking the TCP lenght
+                                    write2TextboxFromSubprocess(richTextBox1, "TCP Length OK");
+                                    if (buffer[6] == ModbusID)
+                                    {
+                                        write2TextboxFromSubprocess(richTextBox1, "ModbusID OK");
+                                        if (buffer[7] == 0x04)//checking the function
+                                        {
+                                            write2TextboxFromSubprocess(richTextBox1, "Function OK");
+                                            Int16 ExpectedDataBytes = (Int16)(RegQty * 2);
+                                            if (buffer[8] == ExpectedDataBytes)//Expected data bytes
+                                            {
+                                                write2TextboxFromSubprocess(richTextBox1, "Data-bytes OK");
+                                                //buffer[9] --reg 12 temp float little endian
+                                                //buffer[10]
+                                                //buffer[11]--reg 13
+                                                //buffer[12]
+                                                //buffer[13]--14-- pressure
+                                                //buffer[14]
+                                                //buffer[15]--15
+                                                //buffer[16]
+                                                //buffer[17]--16--flow
+                                                //buffer[18]
+                                                write2TextboxFromSubprocess(richTextBox1, $"The flow is {buffer[17] * 256 + buffer[18]}");
+
+                                                float temperature = BitConverter.ToSingle(buffer, 9);
+                                                write2TextboxFromSubprocess(richTextBox1, $"The temperature is {temperature}");
+
+                                                byte[] presureBuffer = new byte[4];
+                                                presureBuffer[0] = buffer[16];
+                                                presureBuffer[1] = buffer[15];
+                                                presureBuffer[2] = buffer[14];
+                                                presureBuffer[3] = buffer[13];
+                                                float pressure = BitConverter.ToSingle(presureBuffer, 0);
+                                                write2TextboxFromSubprocess(richTextBox1, $"The pressure is {pressure}");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            write2TextboxFromSubprocess(richTextBox1, "New message");
+                        }
+                    }
+                }
+                else
+                {
+                    write2TextboxFromSubprocess(richTextBox1, "connection failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                write2TextboxFromSubprocess(richTextBox1, ex.ToString());
+            }
+            finally
+            {
+                if (client != null && client.Connected)
+                {
+                    NetworkStream stream = client.GetStream();
+                    stream.Close();
+                    client.Close();
+                }
+                enableButtonFromSubprocess(button1);
+            }
+        }
+        Int16 TransactionID = 0;
+        Int16 MessageLength = 6;
+        byte ModbusID = 17;
+        Int16 Address = 12;
+        Int16 RegQty = 5;
+        private void sendMessage()
+        {
+            if (UIavilable && client != null && client.Connected)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] bytes2send = new byte[12];
+                bytes2send[0] = (byte)(TransactionID >> 8);
+                bytes2send[1] = (byte)(TransactionID);
+                bytes2send[2] = 0X00;//Protocol ID
+                bytes2send[3] = 0X00;//Protocol ID
+                bytes2send[4] = (byte)(MessageLength >> 8);
+                bytes2send[5] = (byte)(MessageLength);
+                bytes2send[6] = ModbusID;
+                bytes2send[7] = 0x04;//function 4
+                bytes2send[8] = (byte)(Address >> 8);
+                bytes2send[9] = (byte)(Address);
+                bytes2send[10] = (byte)(RegQty >> 8);
+                bytes2send[11] = (byte)(RegQty);
+                stream.Write(bytes2send);
+                TransactionID++;
+            }
+        }
+        private void disconnectClient()
+        {
+            keepConnection = false;
+            if (client != null)
+                client.Close();
+        }
+    }
+}
